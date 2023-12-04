a76255626c7558c20691d7b3e0c40d69664bc183
rakib_iu_eee <rakib1415.acb@gmail.com>
2023-12-04 18:50:32 +0600

    styles:some style changes

--- components/destinations/components/Banner.jsx
+++ components/destinations/components/Banner.jsx
@@ -37,7 +37,7 @@ const Banner = ({slug}) => {
         </div>
         <div className="absolute d-flex justify-end items-end col-12 h-full z-1 px-10 py-10">
           <button className="button -md -blue-1 bg-white text-dark-1 text-14 fw-500">
-            See All 90 Photos
+            See All Photos
           </button>
         </div>
       </div>
