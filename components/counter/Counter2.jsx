059a9467c79879a4ae83337b06c586e68c0fd891
rakib_iu_eee <rakib1415.acb@gmail.com>
2023-12-13 12:26:47 +0600

    about page mobile responsive added

--- components/counter/Counter2.jsx
+++ components/counter/Counter2.jsx
@@ -1,13 +1,13 @@
 const Counter2 = () => {
   return (
     <div className="row y-gap-30 text-dark-1">
-      <div className="col-sm-5 col-6">
+      <div className="col-sm-5 col-6 d-flex flex-column justify-content-center align-items-center align-items-sm-start justify-content-sm-start">
         <div className="text-30 lh-15 fw-600">100K+</div>
         <div className="lh-15">Happy People</div>
       </div>
       {/* End .col */}
 
-      <div className="col-sm-5 col-6">
+      <div className="col-sm-5 col-6 d-flex flex-column justify-content-center align-items-center align-items-sm-start justify-content-sm-start">
         <div className="text-30 lh-15 fw-600">4.88</div>
         <div className="lh-15">Overall rating</div>
         <div className="d-flex x-gap-5 items-center pt-10">
